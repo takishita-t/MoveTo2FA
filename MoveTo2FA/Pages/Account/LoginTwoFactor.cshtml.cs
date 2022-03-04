b96b0e29daa3a989ef8fc048105fa68abf132c28
Takishita.t <66511018+takishita-t@users.noreply.github.com>
2022-03-04 09:15:48 +0900

    プロジェクト ファイルを追加します。

---
+++ MoveTo2FA/Pages/Account/LoginTwoFactor.cshtml.cs
@@ -0,0 +1,89 @@
+using MailKit.Security;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MimeKit;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoveTo2FA.Pages.Account
+{
+    public class LoginTwoFactorModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        [BindProperty]
+        public EmailMFA EmailMFA { get; set; }
+
+        public LoginTwoFactorModel(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager
+            )
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.EmailMFA = new EmailMFA();
+        }
+
+        public async Task OnGetAsync(string email, string rememberMe)
+        {
+            //Generate the code 
+            var user = await userManager.FindByEmailAsync(email);
+            var securityCode = await userManager.GenerateTwoFactorTokenAsync(user, "Email");
+
+            //Send to the user
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("", "test@example.com"));
+
+            emailMessage.To.Add(new MailboxAddress("test@test.com", "test@test.com"));
+
+            emailMessage.Subject = "My Web App's OTP";
+
+            emailMessage.Body = new TextPart("plain") { Text = $"Please use this code as the OTP: {securityCode}" };
+
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                await client.ConnectAsync("localhost", 1025, SecureSocketOptions.Auto);
+                //await client.AuthenticateAsync(_sendMailParams.User, _sendMailParams.Password);
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+            }
+
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var result = await signInManager.TwoFactorSignInAsync("Email",
+                this.EmailMFA.SecurityCode,
+                this.EmailMFA.RememberMe,
+                false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToPage("/Index");
+            }
+            else
+            {
+                if (result.IsLockedOut)
+                {
+                    ModelState.AddModelError("Login2FA", "You are locked out.");
+                }
+                else
+                {
+                    ModelState.AddModelError("Login2FA", "Failed to login.");
+                }
+
+                return Page();
+            }
+        }
+    }
+    public class EmailMFA
+    {
+        [Required]
+        [Display(Name = "Security Code")]
+        public string SecurityCode { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ MoveTo2FA/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,95 @@
+using MailKit.Security;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MimeKit;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Encodings.Web;
+using System.Net;
+using System.Net.Mail;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+
+namespace MoveTo2FA.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public RegisterModel(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [BindProperty]
+        public RegisterViewModel RegisterViewModel { get; set; }
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            //validateing Email address
+
+            //Create the userÅ@ìoò^éûÇÃê›íË
+            var user = new IdentityUser
+            {
+                Email = RegisterViewModel.Email,
+                UserName = RegisterViewModel.Email,
+                TwoFactorEnabled = true
+            };
+
+            var result = await this.userManager.CreateAsync(user, RegisterViewModel.Password);
+
+            if (result.Succeeded)
+            {
+                var confirmationToken = await this.userManager.GenerateEmailConfirmationTokenAsync(user);
+                var confirmationLink = Url.PageLink(pageName: "/Account/ConfirmEmail",
+                    values: new { userId = user.Id, token = confirmationToken });
+                //send mail
+                var emailMessage = new MimeMessage();
+
+                emailMessage.From.Add(new MailboxAddress("", "test@example.com"));
+
+                emailMessage.To.Add(new MailboxAddress("test@test.com", "test@test.com"));
+
+                emailMessage.Subject = "Confirm your email";
+
+                emailMessage.Body = new TextPart("plain") { Text = $"Confirm your email{confirmationLink}"};
+
+
+
+                using (var client = new MailKit.Net.Smtp.SmtpClient())
+                {
+                    await client.ConnectAsync("localhost", 1025, SecureSocketOptions.Auto);
+                    //await client.AuthenticateAsync(_sendMailParams.User, _sendMailParams.Password);
+                    await client.SendAsync(emailMessage);
+                    await client.DisconnectAsync(true);
+                }
+
+                //return RedirectToPage("/Account/Login");
+                return Page();
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                return Page();
+            }
+        }
+    }
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invaild email address.")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(dataType: DataType.Password)]
+        public string Password { get; set; }
+    }
+}
