a65ee4d860fa9b901732d41265887f81777919ac
pt.ta-takishita <pt.ta-takishita@tgl.co.jp>
2022-10-19 17:52:02 +0900

    コメント修正

--- MoveTo2FA/Services/IMailSender.cs
+++ MoveTo2FA/Services/IMailSender.cs
@@ -4,6 +4,8 @@ namespace MoveTo2FA.Services
     public interface IMailSender
     {
         Task Execute(string subject, string message, string email);
+
+        //Send Email
         Task SendEmailAsync(string email, string subject, string message);
     }
 }
